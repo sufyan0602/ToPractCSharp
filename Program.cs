c862ebf85ed30285d51dcd4e1f7f6da3c72ee17e
Sufyan Alikhan <sufyannalikhan@gmail.com>
2025-05-21 01:41:11 -0400

    #day1 of cHARP

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        Console.WriteLine("What is the Weather like today? [Sunny, Rainy, Snowy]");
+        
+        string? nameInput = Console.ReadLine();
+        string name = Nameinput ?? "Unkown"
+
+
+        if(nameInput == "Sunny")
+        {
+            Console.WR
+        }
+
+
+
+    }
+}
